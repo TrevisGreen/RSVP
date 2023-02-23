2451e1c0fa228461b3947a012eb5c3c73a1d7446
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 14:24:29 -0600

    Created ConnectionPK class

--- src/main/java/com/RSVP/rsvp/model/ConnectionPK.java
+++ src/main/java/com/RSVP/rsvp/model/ConnectionPK.java
@@ -1,4 +1,84 @@
 package com.RSVP.rsvp.model;
 
-public class ConnectionPK {
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class ConnectionPK implements Serializable {
+
+    private String userid;
+    private String providerId;
+    private String providerUserId;
+
+    // Default constructor
+    public ConnectionPK() {
+    }
+
+    // parameterized constructor
+    public ConnectionPK(String userid, String providerId, String providerUserId) {
+        this.userid = userid;
+        this.providerId = providerId;
+        this.providerUserId = providerUserId;
+    }
+
+    // Getters and Setters
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getProviderUserId() {
+        return providerUserId;
+    }
+
+    public void setProviderUserId(String providerUserId) {
+        this.providerUserId = providerUserId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.userid);
+        hash = 23 * hash + Objects.hashCode(this.providerId);
+        hash = 23 * hash + Objects.hashCode(this.providerUserId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) {
+            return false;
+        }
+        if(getClass() != obj.getClass()) {
+            return false;
+        }
+        final ConnectionPK other = (ConnectionPK) obj;
+        if(!Objects.equals(this.userid, other.userid)) {
+            return false;
+        }
+        if(!Objects.equals(this.providerId, other.providerId)) {
+            return false;
+        }
+        return Objects.equals(this.providerUserId, other.providerUserId);
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectionPK{" + "userid=" + userid + ", providerId=" + providerId + ", providerUserId=" + providerUserId + '}';
+    }
 }
