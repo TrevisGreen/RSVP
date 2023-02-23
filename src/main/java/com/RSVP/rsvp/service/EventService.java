7e530186d6532b6241c6673f4ea217565675fc31
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 17:20:54 -0600

    Created EventService interface

--- src/main/java/com/RSVP/rsvp/service/EventService.java
+++ src/main/java/com/RSVP/rsvp/service/EventService.java
@@ -1,4 +1,21 @@
 package com.RSVP.rsvp.service;
 
+import com.RSVP.rsvp.model.Event;
+
+import java.io.IOException;
+import java.util.Map;
+
 public interface EventService {
+
+    public Map<String, Object> list(Map<String, Object> params);
+
+    public Event createOrUpdate(Event event) throws IOException;
+
+    public Event get(String eventId);
+
+    public Event getByCode(String code);
+
+    public String delete(String eventId, String name);
+
+    public boolean isNotUniqueCode(String code);
 }
