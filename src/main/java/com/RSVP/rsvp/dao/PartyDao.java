63220ac0792cc46f96c88691eab8c6bfa2a95f9f
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 16:21:34 -0600

    Created PartDao interface

--- src/main/java/com/RSVP/rsvp/dao/PartyDao.java
+++ src/main/java/com/RSVP/rsvp/dao/PartyDao.java
@@ -1,4 +1,15 @@
 package com.RSVP.rsvp.dao;
 
+import com.RSVP.rsvp.model.Event;
+import com.RSVP.rsvp.model.Party;
+
+import java.util.List;
+
 public interface PartyDao {
+
+    public Integer getAllotedSeats(Party party);
+
+    public Party created(Party party);
+
+    public List<Party> findallByEvent(Event event);
 }
