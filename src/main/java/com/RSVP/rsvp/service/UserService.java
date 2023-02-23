3a184685e69fbcabc8c4cb311ab0db3cdc725091
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 11:51:53 -0600

    Created UserService interface

--- src/main/java/com/RSVP/rsvp/service/UserService.java
+++ src/main/java/com/RSVP/rsvp/service/UserService.java
@@ -1,4 +1,29 @@
 package com.RSVP.rsvp.service;
 
+import com.RSVP.rsvp.model.Connection;
+import com.RSVP.rsvp.model.Role;
+import com.RSVP.rsvp.model.User;
+
+import java.util.Map;
+
 public interface UserService {
+
+    public User get(String username);
+
+    public User getByOpenId(String openId);
+
+    public User update(User user);
+
+    public User create(User user);
+
+    public Role getRole(String authority);
+
+    public Connection getConnection(String username);
+
+    public Map<String, Object> list(Map<String, Object> params);
+
+    public User get(Long userId);
+
+    public String delete(Long userId);
+
 }
