32fb30641f2ff44eb692e5d1f0594c5455093ba8
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 14:47:40 -0600

    Created PartyService interface

--- src/main/java/com/RSVP/rsvp/service/PartyService.java
+++ src/main/java/com/RSVP/rsvp/service/PartyService.java
@@ -1,4 +1,14 @@
 package com.RSVP.rsvp.service;
 
+import com.RSVP.rsvp.model.Event;
+import com.RSVP.rsvp.model.Party;
+import com.RSVP.rsvp.model.User;
+
+import java.util.List;
+
 public interface PartyService {
+
+    public Party create(Party party) throws NotEnoughSeatsException;
+
+    public List<Party> findAllByEvent(Event event, User user);
 }
