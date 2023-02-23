c567ac07e96010ef218dc66e74b0852e9c58e50b
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 14:40:19 -0600

    Created Party class

--- src/main/java/com/RSVP/rsvp/model/Party.java
+++ src/main/java/com/RSVP/rsvp/model/Party.java
@@ -1,4 +1,132 @@
 package com.RSVP.rsvp.model;
 
-public class Party {
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "parties")
+public class Party implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Version
+    private Integer version;
+    private String name;
+    private String email;
+    private String phone;
+    private Integer seats = 1;
+    private String comments;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateCreated;
+    @ManyToOne(optional = false)
+    private Event event;
+
+    // Default constructor
+    public Party() {
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Integer seats) {
+        this.seats = seats;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + Objects.hashCode(this.id);
+        hash = 83 * hash + Objects.hashCode(this.version);
+        hash = 83 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) {
+            return false;
+        }
+        if(getClass() != obj.getClass()) {
+            return false;
+        }
+        final Party other = (Party) obj;
+        return Objects.equals(this.name, other.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Party{" + "id=" + id + ", version=" + version + ", name=" + name + ", email=" + email + ", phone=" + phone + ", seats=" + seats + ", comments=" + comments + ", event=" + event + '}';
+    }
 }
