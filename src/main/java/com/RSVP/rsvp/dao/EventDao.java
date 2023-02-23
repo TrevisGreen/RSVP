5f092513ac42a61dc9ead586786f5183920a2c07
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 17:47:31 -0600

    Created EventDao interface

--- src/main/java/com/RSVP/rsvp/dao/EventDao.java
+++ src/main/java/com/RSVP/rsvp/dao/EventDao.java
@@ -1,4 +1,20 @@
 package com.RSVP.rsvp.dao;
 
+import com.RSVP.rsvp.model.Event;
+
+import java.util.Map;
+
 public interface EventDao {
+
+    public Map<String, Object> list(Map<String, Object> params);
+
+    public Event create(Event event);
+
+    public Event get(String eventId);
+
+    public Event getByCode(String code);
+
+    public void delete(Event event);
+
+    public Event update(Event event);
 }
