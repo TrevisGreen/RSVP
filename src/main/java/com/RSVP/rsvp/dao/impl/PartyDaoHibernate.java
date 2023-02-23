825b7a769c069c153a9321f100e062065d7951a9
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 17:13:51 -0600

    Created PartyDaoHibernate class

--- src/main/java/com/RSVP/rsvp/dao/impl/PartyDaoHibernate.java
+++ src/main/java/com/RSVP/rsvp/dao/impl/PartyDaoHibernate.java
@@ -1,4 +1,44 @@
 package com.RSVP.rsvp.dao.impl;
 
-public class PartyDaoHibernate {
+
+import com.RSVP.rsvp.dao.BaseDao;
+import com.RSVP.rsvp.dao.PartyDao;
+import com.RSVP.rsvp.model.Event;
+import com.RSVP.rsvp.model.Party;
+import org.hibernate.query.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Respository
+@Transactional
+public class PartyDaoHibernate extends BaseDao implements PartyDao {
+
+    @Override
+    public Integer getAllotedSeats(Party party) {
+        Criteria criteria = currentSession().createCriteria(Party.class);
+
+        criteria.createCriteria("event").add(Restrictions.idEq(party.getEvent().getId()));
+        criteria.setProjection(Projection.sum("seats"));
+
+        Long results = (Long) criteria.uniqueResult();
+        log.debug("Result {}", results);
+        if(results == null) {
+            results = 0L;
+        }
+        return  results.intValue();
+    }
+
+    @Override
+    public Party create(Party party) {
+        currentSession().save(party);
+        return party;
+    }
+
+    @Override
+    public List<Party> findAllByEvent(Event event) {
+        Query query = currentSession().createQuery("select p from Party p inner join p.event e where e.id = :eventId");
+        query.setString("eventId",event.getId());
+        return query.list();
+    }
 }
