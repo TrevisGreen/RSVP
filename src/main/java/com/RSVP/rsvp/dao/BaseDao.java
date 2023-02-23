ad2e3f50bc19cd235137b9e5adcf663c5a2216ae
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 12:12:53 -0600

    Created BaseDao class

--- src/main/java/com/RSVP/rsvp/dao/BaseDao.java
+++ src/main/java/com/RSVP/rsvp/dao/BaseDao.java
@@ -1,4 +1,21 @@
 package com.RSVP.rsvp.dao;
 
-public class BaseDao {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public abstract class BaseDao {
+
+    protected final transient Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional
+    protected Session currentSession() {
+        return sessionFactory.getCurrentSession();
+    }
 }
