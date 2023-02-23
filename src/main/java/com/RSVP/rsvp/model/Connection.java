b54e72d7c508add6d1ce7e13e63e1ce0d4569a41
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 14:10:09 -0600

    Created Connection class

--- src/main/java/com/RSVP/rsvp/model/Connection.java
+++ src/main/java/com/RSVP/rsvp/model/Connection.java
@@ -1,4 +1,127 @@
 package com.RSVP.rsvp.model;
 
-public class Connection {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "UserConnection")
+public class Connection implements Serializable {
+
+    @Id
+    private Connection id;
+    private Integer rank;
+    private String displayName;
+    private String profileUrl;
+    private String imageUrl;
+    private String accessToken;
+    private String secret;
+    private String refreshToken;
+    private Long expireTime;
+
+    // Default constructor
+    public Connection() {
+    }
+
+    // Getters and Setters
+
+    public Connection getId() {
+        return id;
+    }
+
+    public void setId(Connection id) {
+        this.id = id;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Long getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(Long expireTime) {
+        this.expireTime = expireTime;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) {
+            return false;
+        }
+        if(getClass() != obj.getClass()) {
+            return false;
+        }
+        final Connection other = (Connection) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Connection{" + "id=" + id + ", rank=" + rank + ", displayName=" + displayName + ", profileUrl=" + profileUrl + ", imageUrl=" + imageUrl + ", accessToken=" + accessToken + ", secret=" + secret + ", refreshToken=" + refreshToken + ", expireTime=" + expireTime + '}';
+    }
 }
