ab33d7c6bf9bfa451b6819a4454f3e87cd029d14
TrevisGreen <Trevis.Green@gmail.com>
2023-02-24 08:21:07 -0600

    Created ComponentConfig class

--- src/main/java/com/RSVP/rsvp/config/ComponentConfig.java
+++ src/main/java/com/RSVP/rsvp/config/ComponentConfig.java
@@ -1,4 +1,16 @@
 package com.RSVP.rsvp.config;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "com.RSVP.rsvp.model",
+        "com.RSVP.rsvp.dao",
+        "com.RSVP.rsvp.utils",
+        "com.RSVP.rsvp.service"})
+@PropertySource("classpath:rsvp.properties")
 public class ComponentConfig {
 }
