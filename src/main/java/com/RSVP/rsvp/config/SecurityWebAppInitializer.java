2ce01a1c80483eba939e0ce591545921a6bf7f32
TrevisGreen <Trevis.Green@gmail.com>
2023-02-24 08:30:07 -0600

    Created SecurityWebAppInitializer class

--- src/main/java/com/RSVP/rsvp/config/SecurityWebAppInitializer.java
+++ src/main/java/com/RSVP/rsvp/config/SecurityWebAppInitializer.java
@@ -1,4 +1,10 @@
 package com.RSVP.rsvp.config;
 
-public class SecurityWebAppInitializer {
+
+import org.springframework.core.annotation.Order;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+@Order(1)
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
 }
