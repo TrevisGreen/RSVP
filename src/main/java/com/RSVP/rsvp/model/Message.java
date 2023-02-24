9591a07830c9891d3bf63382d526a831e9098d55
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 18:03:01 -0600

    Created Message class

--- src/main/java/com/RSVP/rsvp/model/Message.java
+++ src/main/java/com/RSVP/rsvp/model/Message.java
@@ -1,4 +1,108 @@
 package com.RSVP.rsvp.model;
 
-public class Message {
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "messages")
+@NamedQueries({
+        @NamedQuery(name = "findMessageByName", query = "select m from Message m where m.name = :name")
+})
+public class Message implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Version
+    private Integer version;
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name;
+    @NotBlank
+    @Column(nullable = false)
+    private String subject;
+
+    @NotBlank
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    // Default constructor
+    public Message(){
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        hash = 41 * hash + Objects.hashCode(this.version);
+        hash = 41 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Message other = (Message) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" + "id=" + id + ", version=" + version + ", name=" + name + ", subject=" + subject + ", content=" + content + '}';
+    }
 }
