c72d7b0d42ff04eb4a2312d76556277f1cc15fca
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 18:07:01 -0600

    Created MessageDao interface

--- src/main/java/com/RSVP/rsvp/dao/MessageDao.java
+++ src/main/java/com/RSVP/rsvp/dao/MessageDao.java
@@ -1,4 +1,18 @@
 package com.RSVP.rsvp.dao;
 
+import com.RSVP.rsvp.model.Message;
+
+import java.util.Map;
+
 public interface MessageDao {
+
+    public Map<String,Object> list(Map<String, Object> params);
+
+    public Message get(Long messageId);
+
+    public Message update(Message message);
+
+    public Message get(String name);
+
+    public Message create(Message message);
 }
