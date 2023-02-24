4368191f133912e28fecc6dc5683575b6aeacb96
TrevisGreen <Trevis.Green@gmail.com>
2023-02-24 08:38:43 -0600

    Created WebAppInitializer class

--- src/main/java/com/RSVP/rsvp/config/WebAppInitializer.java
+++ src/main/java/com/RSVP/rsvp/config/WebAppInitializer.java
@@ -1,4 +1,34 @@
 package com.RSVP.rsvp.config;
 
-public class WebAppInitializer {
+
+import jakarta.servlet.Filter;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Order(2)
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{SecurityConfig.class, SocialConfig.class, DataConig.class, MailConfig.class, ComponentConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+
+        return new Filter[]{characterEncodingFilter};
+    }
 }
