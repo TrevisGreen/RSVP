26fdb1c0fe5df20f05ffd12d8fbb7fb13daec44f
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 20:02:44 -0600

    Created PasswordsNotEmpty interface

--- src/main/java/com/RSVP/rsvp/validation/PasswordsNotEmpty.java
+++ src/main/java/com/RSVP/rsvp/validation/PasswordsNotEmpty.java
@@ -1,4 +1,32 @@
 package com.RSVP.rsvp.validation;
 
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Target( {TYPE,ANNOTATION_TYPE} )
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordsNotEmptyValidator.class)
+@Documented
 public interface PasswordsNotEmpty {
+
+    String message() default "PasswordsNotEmpty";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String triggerFieldName() default "";
+
+    String passwordFieldName() default "";
+
+    String passwordVerificationFieldName() default "";
 }
