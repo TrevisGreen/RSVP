6be16f1af4c5336e8ff1ebc58a8c557ccc7872e5
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 19:04:41 -0600

    Created Constants class

---
+++ src/main/java/com/RSVP/rsvp/utils/Constants.java
@@ -0,0 +1,22 @@
+package com.RSVP.rsvp.utils;
+
+public class Constants {
+
+    public static final String LOGGED_USER = "LOGGED_USER";
+
+    public static final String CREATE = "CREATE";
+
+    public static final String SIGN_UP = "SIGN_UP";
+
+    public static final String CODE = "CODE";
+
+    public static final String THANKS = "THANKS";
+
+    public static final String BACKGROUND_ID = "BACKGROUND_ID";
+
+    public static final String NEW = "NEW";
+
+    public static final String DRAFT = "DRAFT";
+
+    public static final String PUBLISHED = "PUBLISHED";
+}
