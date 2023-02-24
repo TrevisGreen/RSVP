e0d6608cedf5e1923122f4166b07507cbd167d26
TrevisGreen <Trevis.Green@gmail.com>
2023-02-23 18:13:46 -0600

    Created MessageService interface

--- src/main/java/com/RSVP/rsvp/service/MessageService.java
+++ src/main/java/com/RSVP/rsvp/service/MessageService.java
@@ -1,4 +1,16 @@
 package com.RSVP.rsvp.service;
 
+import com.RSVP.rsvp.model.Message;
+
+import java.util.Map;
+
 public interface MessageService {
+
+    public Map<String, Object> list(Map<String, Object> params);
+
+    public Message get(Long messageId);
+
+    public Message update(Message message);
+
+    public Message get(String name);
 }
