96aabdc265d670e6fccb6e5338a2eb542b1eafa8
TrevisGreen <Trevis.Green@gmail.com>
2023-02-25 14:14:37 -0600

    bug fix

--- src/main/java/com/RSVP/rsvp/config/SecurityConfig.java
+++ src/main/java/com/RSVP/rsvp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.RSVP.rsvp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @EnableWebMvcSecurity
