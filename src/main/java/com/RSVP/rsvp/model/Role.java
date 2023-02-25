b6f5ce2430eed35d7e56eb40657effe6d5eb0dad
TrevisGreen <Trevis.Green@gmail.com>
2023-02-25 13:17:07 -0600

    Created Role class

--- src/main/java/com/RSVP/rsvp/model/Role.java
+++ src/main/java/com/RSVP/rsvp/model/Role.java
@@ -1,4 +1,65 @@
 package com.RSVP.rsvp.model;
 
-public class Role {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Role implements Serializable, GrantedAuthority {
+
+    @Id
+    private String authority;
+
+    // Default constructor
+    public Role() {
+    }
+
+    // Parameterized constructor
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    // Getters and Setters
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.authority);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role other = (Role) obj;
+        return Objects.equals(this.authority, other.authority);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{authority=" + authority + '}';
+    }
 }
