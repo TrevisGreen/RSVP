2e74fbb0bb83a3315cdec7b53140e48b7331514d
TrevisGreen <Trevis.Green@gmail.com>
2023-02-26 13:04:17 -0600

    bug fix

--- src/main/java/com/RSVP/rsvp/service/impl/EventServiceImpl.java
+++ src/main/java/com/RSVP/rsvp/service/impl/EventServiceImpl.java
@@ -8,6 +8,7 @@ import com.RSVP.rsvp.model.Role;
 import com.RSVP.rsvp.model.User;
 import com.RSVP.rsvp.service.BaseService;
 import com.RSVP.rsvp.service.EventService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
