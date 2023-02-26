35a3708827400cd826f04e4c327c1c9ca00c9277
TrevisGreen <Trevis.Green@gmail.com>
2023-02-26 13:22:03 -0600

    bug fix

--- src/main/java/com/RSVP/rsvp/config/WebAppInitializer.java
+++ src/main/java/com/RSVP/rsvp/config/WebAppInitializer.java
@@ -1,16 +1,17 @@
 package com.RSVP.rsvp.config;
 
 
-import jakarta.servlet.Filter;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 @Order(2)
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
-    protected Class<?>[] getRootConfigClasses() {
+    public Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{SecurityConfig.class, SocialConfig.class, DataConfig.class, MailConfig.class, ComponentConfig.class};
     }
 
@@ -28,7 +29,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     protected Filter[] getServletFilters() {
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
-
         return new Filter[]{characterEncodingFilter};
     }
+
 }
